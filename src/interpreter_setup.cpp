d1a5f33068d931ae879e99b0f8f87e3998568fa9
Viggo Lekdorf <viggo@lekdorf.com>
2026-04-22 13:49:44 +0200

    catch invalid hex chars

--- src/interpreter_setup.cpp
+++ src/interpreter_setup.cpp
@@ -1421,7 +1421,7 @@ Interpreter::Interpreter() {
                 if (c >= '0' && c <= '9') return c - '0';
                 if (c >= 'a' && c <= 'f') return c - 'a' + 10;
                 if (c >= 'A' && c <= 'F') return c - 'A' + 10;
-                return 0;
+                throw RuntimeError(std::string("bytes.fromHex() invalid hex character: '") + c + "'", 0);
             };
             std::string result;
             for (size_t i = 0; i < hex.size(); i += 2)
